c974d7ee8e6e143cbb55d50edb2d0246daa831a9
maruhachi <konchan1129@gmail.com>
2019-04-11 21:00:41 +0900

    :cat: serval-buttonをベースにSSEをもらって関数を呼ぶ部分を移植. 関数とイベントハンドラの設定を分離.

--- sync-play.js
+++ sync-play.js
@@ -1,25 +1,109 @@
+/**
+ * 共通変数
+ */
 var v = document.getElementById("video");
 
-function getDuration() {
-	//動画の長さ（秒）を表示
-	document.getElementById("nagasa").innerHTML = v.duration;
-}
 
-function playVideo() {
-	//再生完了の表示をクリア
-	document.getElementById("ended").innerHTML = "";
-	//動画を再生
-	v.play();
-	//現在の再生位置（秒）を表示
-	v.addEventListener("timeupdate", function(){
-		document.getElementById("currentTime").innerHTML = v.currentTime;
-	}, false);
-	//再生完了を知らせる
-	v.addEventListener("ended", function(){
-		document.getElementById("ended").innerHTML = "動画の再生が完了しました。";
-	}, false);
-}
+/**
+ * イベントハンドラ定義
+ */
+const handlers = {
 
-document.getElementById("player").addEventListener("click", function(){
-    playVideo();
-})
+    //動画の長さ（秒）を表示
+    getDuration: function () {
+        document.getElementById("nagasa").innerHTML = v.duration;
+    },
+
+    // サーバと同期
+    synchro: function () {
+
+        const eventSource = new EventSource('./serval/');
+        eventSource.onopen = function () {
+            document.querySelector('#logtxt').value += 'eventSource open\n';
+        };
+        eventSource.onerror = function () {
+            document.querySelector('#logtxt').value += 'eventSource error\n';
+        };
+        eventSource.onmessage = function (event) {
+            console.log(event);
+        };
+        eventSource.addEventListener('tanoshi-count', function (event) {
+            const eventInfo = {
+                // たのしー！
+                data: event.data
+            };
+            document.querySelector('#logtxt').value += 'event=' + JSON.stringify(eventInfo) + "\n";
+            document.querySelector('#tanoshiCount').innerHTML = eventInfo.data + "たのしー！";
+            handlers.servalPopup();
+        }, false);
+        window.currentEventSource = eventSource;
+    },
+
+    playVideo: function () {
+        //再生完了の表示をクリア
+        document.getElementById("ended").innerHTML = "";
+        //動画を再生
+        v.play();
+
+    },
+
+    // 動画を一時停止
+    stopVideo: function(){
+        v.pause();
+    },
+
+
+    updateTime: function(){
+        document.getElementById("currentTime").innerHTML = v.currentTime;
+    },
+
+    videoEnded: function(){
+        document.getElementById("ended").innerHTML = "動画の再生が完了しました。";
+    },
+
+
+    servalPopup: function () {
+        // 何秒か経ったらクラス外す（フレームアウト
+        setTimeout(function () {
+            target.className = "";
+        }, 2100);
+
+    },
+
+    tanoshi: function () {
+        fetch("./tanoshi/")
+            .then((response) = > response.text()
+    )
+    .
+        then((text) = > document.querySelector('#logtxt').value += "たのしー！\n"
+    )
+        ;
+    }
+
+};
+
+/**
+ * 画面ロード時のイベント
+ */
+
+window.onload = function () {
+    handlers.getDuration();
+};
+
+/**
+ * イベントハンドラのセット
+ */
+// サーバ同期開始
+document.getElementById('syncButton').addEventListener('click', handlers.synchro);
+
+// 再生
+document.getElementById("player").addEventListener("click", handlers.playVideo);
+document.getElementById("stoper").addEventListener("click", handlers.stopVideo);
+
+// 動画イベント
+
+//再生完了を知らせる
+v.addEventListener("timeupdate", handlers.updateTime, false);
+
+//現在の再生位置（秒）を表示
+v.addEventListener("ended", handlers.videoEnded, false);
